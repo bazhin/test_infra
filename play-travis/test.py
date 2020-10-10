b02ee707c43f0c9dadc2d21efa898b819bfc62bc
bazhin <bazhin@inbox.ru>
2020-10-10 21:18:13 +0300

    изменил на очень простой код, для тестов

--- play-travis/test.py
+++ play-travis/test.py
@@ -1,23 +1,7 @@
-import pytest
+# content of test_sample.py
+def inc(x):
+    return x + 1
 
 
-def setup_module(module):
-    # init_something()
-    pass
-
-
-def teardown_module(module):
-    # teardown_something()
-    pass
-
-
-def test_upper():
-    assert 'foo'.upper() == 'FOO'
-
-
-def test_isupper():
-    assert 'FOO'.isupper()
-
-
-def test_failed_upper():
-    assert 'Foo'.upper() == 'FOO'
+def test_answer():
+    assert inc(3) == 5
